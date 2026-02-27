2acff667e6bb51058956a10a9e956ef82f7f18ec
boyke121212 <boyke1212@gmail.com>
2026-02-27 07:15:16 +0000

    modified:   app/Config/Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'ada deh',
-        'password'     => 'boyike',
+        'password'     => 'boyike nih',
         'database'     => 'database',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
